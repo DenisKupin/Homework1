f51c54e0a26a23d3fdd35898c1f63632465cdd25
Denis Kupin <den17a6@gmail.com>
2020-01-02 15:01:48 +0200

    hw1.1-comments

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ var numbers = [ 254, 115, 78, 25, 91, 45, 37 ]
 
 var i
 
-for (i=0; i<numbers.length;i++){
-    if(numbers[i]>50){
+for (i=0; i<numbers.length;i++){ //цикл для проверки цифр в массиве
+    if(numbers[i]>50){ //проверка чисел которые больше 50
         console.log(numbers[i])
         alert (numbers[i])
     }
